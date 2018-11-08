a543d3aa3ee1ee8d06c8ab1e7e1ec85ebe3b9388
Leo <leotsl@gmail.com>
2018-11-09 00:36:50 +0800

    register

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,30 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.render('user', {
+        title: 'User Information',
+        user:{
+            userID: '1'
+        }
+    });
+});
+router.post('/Register', function(req, res, next) {
+    res.render('register', {
+        title: 'User Register',
+        user:{
+            userName: req.body.user_name,
+            userEmail: req.body.user_email
+        }
+    });
 });
 
+router.get('/Register', function(req, res, next){
+
+})
+
+router.get('/Profile/:id', function(req, res, next){
+    console.log(res.body);
+    let userID = req.params.id;
+    res.send("userID :" + userID + " Profile");
+});
 module.exports = router;
